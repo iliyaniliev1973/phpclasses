5dcc32e93078d99ba44943b73ca9f775ade9ec73
iliyaniliev1973 <ilian.k.iliev@gmail.com>
2015-05-10 09:19:42 +0300

    homework 1 php

---
+++ 04-php/homework 1.php
@@ -0,0 +1,21 @@
+<style>
+body {
+    background-color: #FF3333;
+	</style>
+
+<?php
+		$a = 15;
+		
+		
+	if($a == 15){
+		echo "az sym 15";
+	} 
+	if($a > 26){
+		echo "po-golqmo ot 26"; 
+	} 
+	echo "<br/>"; //break to new line.
+
+	if($a <= 64){
+		echo "po-malko ili ravno na 64";
+	} 
+ ?>
